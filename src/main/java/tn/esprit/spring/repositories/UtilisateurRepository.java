f66f0290242a9ec1010fb74c4336b238ecd73471
Sirine NAIFAR <sirine.naifer@esprit.tn>
2025-11-11 19:56:22 +0100

    JPQL

--- src/main/java/tn/esprit/spring/repositories/UtilisateurRepository.java
+++ src/main/java/tn/esprit/spring/repositories/UtilisateurRepository.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.esprit.spring.entities.Utilisateur;
 
@@ -22,11 +24,24 @@ public interface UtilisateurRepository extends
 
     List<Utilisateur> queryByNom(String nom);
 
+    @Query("SELECT u FROM Utilisateur u WHERE u.nom = ?1")
+    List<Utilisateur> recupJPQL(String nom);
+
+    @Query(value = "SELECT * FROM user WHERE lastName =:n", nativeQuery = true)
+    List<Utilisateur> recupSQL(@Param("n") String nom);
+
     //select * from User where nom=Ahmed ou AHMED ou aHMED
     List<Utilisateur> findByNomIgnoreCase(String nom);
 
     //select * from User where nom=... and prenom = ...
     List<Utilisateur> findByNomAndPrenom(String n, String p);
+
+    @Query("SELECT u FROM Utilisateur u WHERE u.nom = ?2 AND u.prenom = ?1")
+    List<Utilisateur> recupJPQL1(String prenom, String nom);
+
+    @Query(value = "SELECT * FROM user WHERE lastName =:n AND firstName =:p",
+            nativeQuery = true)
+    List<Utilisateur> recupSQL1(@Param("n") String nom, @Param("p") String prenom);
 
     //select * from User where nom=... or prenom = ...
     List<Utilisateur> findByNomOrPrenom(String n, String p);
@@ -71,17 +86,20 @@ public interface UtilisateurRepository extends
     List<Utilisateur> findByCinGreaterThanAndCinLessThan(long min, long max);
 
     List<Utilisateur> findByCinBetween(long min, long max);
+
     List<Utilisateur> findByCinIsBetween(long min, long max);
-    
+
     // select * from User where dateNaissance=...
     List<Utilisateur> getByDateNaissance(LocalDate localDate);
 
     // select * from User where dateNaissance>...
     List<Utilisateur> getByDateNaissanceAfter(LocalDate date);
+
     List<Utilisateur> getByDateNaissanceIsAfter(LocalDate date);
 
     // select * from User where dateNaissance<...
     List<Utilisateur> getByDateNaissanceBefore(LocalDate date);
+
     List<Utilisateur> getByDateNaissanceIsBefore(LocalDate date);
 
 }
